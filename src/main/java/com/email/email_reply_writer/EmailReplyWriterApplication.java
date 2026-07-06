89727f2ddd450dd90e62db652e2b42f0658ba177
Vishal Keshari <kesharivishal611@gmail.com>
2026-07-07 03:04:28 +0530

    Initial commit

---
+++ src/main/java/com/email/email_reply_writer/EmailReplyWriterApplication.java
@@ -0,0 +1,13 @@
+package com.email.email_reply_writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailReplyWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailReplyWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email/email_reply_writer/EmailReplyWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.email_reply_writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailReplyWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
